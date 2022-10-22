206b47e98e2363cfd4ab822301ef384484433e77
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-22 00:20:34 -0300

    Add files via upload

--- pneumonia_paises_capitalistas_comunistas.R
+++ pneumonia_paises_capitalistas_comunistas.R
@@ -30,3 +30,32 @@ library(ggthemes)
 pne <- read.csv("pneumonia-death-rates-age-standardized.csv")
 view(pne)
 names(pne)
+
+# Manipular dados --------------------------------------------------------------------------------------------------------------------------
+
+pne <- pne %>%
+  select(-Code) %>%
+  rename(taxa_mortes_pneu = Deaths...Lower.respiratory.infections...Sex..Both...Age..Age.standardized..Rate.) %>%
+  view()
+
+pne1 <- pne %>%
+  filter(Entity %in% c("United States", "Japan", "Germany",
+                       "China", "Cuba", "North Korea")) %>%
+  group_by(Entity) %>%
+  summarise(media = mean(taxa_mortes_pneu),
+            sd = sd(taxa_mortes_pneu), n = n(),
+            se = sd/sqrt(n)) %>%
+  view()
+
+pne2 <- pne %>%
+  filter(Entity %in% c("United States", "Japan", "Germany",
+                       "China", "Cuba", "North Korea")) %>%
+  view()
+
+pne3 <- pne %>%
+  filter(Entity %in% c("United States", "China")) %>%
+  view()
+
+# Gráficos ---------------------------------------------------------------------------------------------------------------------------------
+
+
