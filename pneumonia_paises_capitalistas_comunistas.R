401104b2524f64d6cc63b0d32d4b8bc9b5f6e272
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-22 01:49:16 -0300

    Add files via upload

--- pneumonia_paises_capitalistas_comunistas.R
+++ pneumonia_paises_capitalistas_comunistas.R
@@ -69,12 +69,37 @@ ggplot(pne1, aes(x = fct_reorder(Entity, media),
                              "#DDCC77", "#117733",
                              "#332288", "#AA4499")) +
   scale_y_continuous(expand = expansion(mult = c(0,0))) +
+  scale_x_discrete(labels = c("Alemanha", "Estados Unidos", "Japão",
+                              "China", "Cuba", "Coreia do Norte")) +
   labs(x = "Países", y = "Taxa de mortes por pneumonia (%)") +
   theme_ipsum(axis_text_size = 14, axis_title_size = 16) +
   theme(legend.position = "none",
         axis.text = element_text(color = "black"))
 
+ggplot(pne2, aes(x = Year, y = taxa_mortes_pneu,
+                 group = Entity, col = Entity)) +
+  geom_point() +
+  geom_line() +
+  scale_color_manual(values = c("#88CCEE", "#CC6677",
+                             "#DDCC77", "#117733",
+                             "#332288", "#AA4499"),
+                     labels = c("China", "Cuba", "Alemanha",
+                                "Japão", "Coreia do Norte", "Estados Unidos")) +
+  labs(x = "Tempo (anos)", y = "Taxa de mortes por pneumonia (%)",
+       color = "Países") +
+  theme_ipsum(axis_text_size = 14, axis_title_size = 16) +
+  theme(axis.text = element_text(color = "black"))
 
+c4a("dark2", 2)
 
-
+ggplot(pne3, aes(x = Year, y = taxa_mortes_pneu,
+                 group = Entity, col = Entity)) +
+  geom_line(size = 2.2) +
+  scale_color_manual(values = c("#1B9E77", "#D95F02")) +
+  labs(x = "Tempo (anos)", y = "Taxa de mortes por pneumonia (%)",
+       color = "Países") +
+  theme_hc() +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(color = "black", size = 15),
+        legend.text = element_text(size = 12))
 
