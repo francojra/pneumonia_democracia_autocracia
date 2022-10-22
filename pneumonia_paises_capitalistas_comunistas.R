15a9be4e2eb31636f075ec4f1144b9bde633b98c
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-21 23:47:21 -0300

    Add files via upload

---
+++ pneumonia_paises_capitalistas_comunistas.R
@@ -0,0 +1,24 @@
+
+# Pneumonia em países capitalistas e comunistas --------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 21/10/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/pneumonia -----------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Em 2019, houve 2,5 milhões de pessoas que morreram de peneumonia. Quase um terço das
+### vítimas foram crianças com menos de 5 anos, essa é a principal causa de morte para
+### crianças de até 5 anos.
+
+### Pneumonia é uma infecção nos pequenos sacos aéreos do pulmão, chamados de alvéolos.
+### Em uma pessoa com pneumonia, os alvéolos são preenchidos com pus e fluidos, o que torna
+### a respiração dolorosa e reduz a ingestão de oxigênio. Pneumonia é causada por um diferente
+### número de agentes infecciosos, incluindo vírus, bactérias e fungos. 
+
+### Neste registro nós observamos quem está sofrendo de pneumonia e porquê - e o que nós
+### podemos fazer para reduzir o número de pessoas morrendo dessa doença.
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+
+
